ca1735ed35dc83fbecaacd09f0b3a4a3dc9517fe
febef <febef.dev@gmail.com>
2020-11-29 05:44:35 -0300

    fix typo

--- Docker/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting sshd:"
-/usr/sbin/sshd
-echo "Starting nginx in non daemon mode:"
-/usr/sbin/nginx
-echo "Starting node app:"
-node /opt/gitSnak/bin/gitSnak
-echo "Ready!"
